85307e3f72fefe037a9850b8126f86d9909e444f
Arthur Asasira <asasiraarthur@gmail.com>
2024-07-08 21:17:09 +0300

    server

--- src/Mail.java
+++ src/Mail.java
@@ -1,10 +1,47 @@
 import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.*;
 
 //this class shall handle the email sending
 public class Mail {
-//    private final String username = "
+    //    private final String username = "
+    Properties prop = null;
+    Session session = null;
+    Transport transport = null;
+    Message message = null;
 
+    public Mail() {
+        prop = new Properties();
+        prop.put("mail.user", "g4mcms256@gmail.com");
+        prop.put("mail.password", "art272727.");
+        prop.put("mail.smtp.host", "smtp.gmail.com"); // SMTP Host
+        prop.put("mail.smtp.port", "587"); // TLS Port
+        prop.put("mail.smtp.auth", "true"); // Enable authentication
+        prop.put("mail.smtp.starttls.enable", "true"); // Enable STARTTLS
+        session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("g4mcms256@gmail.com", "art272727.");
+            }
+        });
+    }
 
+    public void sendMail(String schoolEmail, String subject, String body) {
+        try {
+            message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(prop.getProperty("mail.user")));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(schoolEmail));
+            message.setSubject(subject);
+            message.setText(body);
+            transport = session.getTransport("smtp");
+            transport.connect(prop.getProperty("mail.smtp.host"), prop.getProperty("mail.user"), prop.getProperty("mail.password"));
+            transport.sendMessage(message, message.getAllRecipients());
+        } catch (MessagingException e) {
+            System.out.println(e.getMessage());
 
+        }
+    }
 }
--- src/Representative.java
+++ src/Representative.java
@@ -18,6 +18,7 @@ public class Representative {
     }
 
     public void validateParticipant() {
+
         String line;
         DBO dbo = new DBO();
         dbo.connect();
@@ -28,20 +29,24 @@ public class Representative {
 ) {
             System.out.println(schoolRegNo);
             String reply;
-            while ((line = bufferedFileReader.readLine()) != null) {
-                // send line to server if the school registration number matches the school registration number of the representative
-                if (line.split(" ")[6].equalsIgnoreCase(schoolRegNo)){
-                    Main.server.printWriter.println(line);
-                    reply=Main.server.reader.readLine();
-                    if (reply.equalsIgnoreCase("yes"))
-                        dbo.insertParticipant(line.split(" "),Main.server.imageToByteArray(line.split(" ")[7]));
-                     else if (reply.equalsIgnoreCase("no") )
-                        dbo.insertRejectedParticipant(line.split(" "),Main.server.imageToByteArray(line.split(" ")[7]));
-                }else
-                //add to arraylist
-                {
-                    System.out.println(line);
-                    participants.add(line);
+            if ((reply=Main.server.reader.readLine()).equalsIgnoreCase("yes")) {
+
+
+                while ((line = bufferedFileReader.readLine()) != null) {
+                    // send line to server if the school registration number matches the school registration number of the representative
+                    if (line.split(" ")[6].equalsIgnoreCase(schoolRegNo)) {
+                        Main.server.printWriter.println(line);
+                        reply = Main.server.reader.readLine();
+                        if (reply.equalsIgnoreCase("yes"))
+                            dbo.insertParticipant(line.split(" "), Main.server.imageToByteArray(line.split(" ")[7]));
+                        else if (reply.equalsIgnoreCase("no"))
+                            dbo.insertRejectedParticipant(line.split(" "), Main.server.imageToByteArray(line.split(" ")[7]));
+                    } else
+                    //add to arraylist
+                    {
+                        System.out.println(line);
+                        participants.add(line);
+                    }
                 }
             }
             System.out.println("clearing participants file");
